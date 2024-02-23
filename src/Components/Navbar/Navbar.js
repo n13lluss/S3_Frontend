d7826edb2e30f7fe8c543afc98c2ebe67aa86549
n13lluss <nielsvanlierop15@gmail.com>
2024-02-23 17:51:53 +0100

    first tries

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src='' alt='' /> 
+      <ul>
+        navlink
+      </ul>
+      <ul>
+
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
