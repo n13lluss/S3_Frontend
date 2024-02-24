e115d903efc631ba3ce3f2a99bc70c82a97c91c2
n13lluss <nielsvanlierop15@gmail.com>
2024-02-24 20:59:28 +0100

    test runs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import Navbar from './Components/Navbar/Navbar'
-import Posts from './Components/Posts/Posts'
-import Footer from './Components/Footer/Footer' 
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
-    <>
-    <div>
-      <Navbar />
-      <Posts classname="Posts"/>
-      <Footer />
-    </div>
-    </>
+    <BrowserRouter>
+    <header>
+      <nav>
+        <h1>JobaRouter</h1>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="about">About</NavLink>
+      </nav>
+    </header>
+      <main>
+        <Routes>
+            <Route index element={<Home />}/>
+            <Route path='about' element={<About />}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
