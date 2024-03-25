ff5bbb88891e40a68e2d1dfbd25af92140ee2603
niels van lierop <nielsvanlierop15@gmail.com>
2024-03-25 17:25:48 +0100

    updated inlog

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import BlogList from './pages/Blog/BlogList';
 import BlogView from './pages/Blog/BlogView';
 import BlogCreate from './pages/Blog/BlogCreate';
 import BlogEdit from './pages/Blog/BlogEdit';
-import LoginPage from './pages/User/LoginPage';
-import Register from './pages/User/Register';
 import './App.css';
 
 function App() {
@@ -40,8 +38,6 @@ function App() {
         <Route path="/blogs/:id" element={<BlogView />} />
         <Route path="/create" element={<BlogCreate />} />
         <Route path="/edit/:id" element={<BlogEdit />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
