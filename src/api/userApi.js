b538d10efcff25b7b2724c24b92212ecbdfcdabe
niels van lierop <nielsvanlierop15@gmail.com>
2024-04-04 10:52:49 +0200

    push

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -4,11 +4,12 @@ const userApi = {
   createUser: async (userData) => {
     console.log(userData);
     try {
-      const response = await api.User.post('/Register', {
+      const response = await api.User.post('/register', {
         name: userData.name,
         email: userData.email,
         IdString: userData.IdString,
       });
+      console.log(response.data);
       return response.data;
     } catch (error) {
       throw error;
